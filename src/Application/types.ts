32d7c2e9d3d7366ac6167b587942fb104ffcce3d
Mangesh Bide <mangeshbide123@gmail.com>
2024-11-13 23:53:47 +0530

    adds application and its service

--- src/Application/types.ts
+++ src/Application/types.ts
@@ -1,12 +1,19 @@
+
+
 export type Status = "accepted" | "rejected"| "pending"
 export type RoundBasePrice = {[key: number]: number}
 export type CreateApplicationDTO = {
     playerId: number,
     auctionId:number,
-  
     roundBasePrice: RoundBasePrice
+
 }
 
+export type ApplicationMeta= {
+    authorityId?: number,
+    authorityComment?: string
+    authorizedOn?: string
+}
 export type EditApplicationDTO = {
     roundBasePrice : RoundBasePrice
 }
